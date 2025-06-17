3ae64d13da46928cce78918eeb4d0473b8c14830
Miguel <miguelhonoriop@gmail.com>
2025-06-17 12:45:41 -0300

    Update Json

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -16,9 +16,11 @@ async function carregarCards() {
     dados[tipo].forEach(card => {
       const div = document.createElement('div');
       div.className = 'card';
+
+       const nomeSemEmojis = card.name.replace(/[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu, '');
       div.innerHTML = `
         <img src="${card.image}" alt="Imagem do card">
-        <p>${card.name}</p>
+        <p>${nomeSemEmojis}</p>
       `;
 
       div.addEventListener('click', () => abrirModal(card));
@@ -49,7 +51,8 @@ function abrirModal(card) {
     
 
   const status = (card.status || 'Unknown').toLowerCase();
-  let color = (status === 'finalizado') ? 'green' : 'red';
+  const color = (status === 'finalizado' || status === 'hiato') ? 'red' : 'green';
+
 
   ps[2].innerHTML = `Status: <span style="color: ${color};">${card.status}</span>`;
   
